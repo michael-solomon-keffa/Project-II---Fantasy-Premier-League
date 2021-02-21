638054ec5ec14a51d79d8cda95af709c1b4cb6a5
AbenezerAtnafu <se.abenezer.atnafu@gmail.com>
2021-02-21 16:10:30 +0300

    fetching managers done

--- src/services/remote-services.js
+++ src/services/remote-services.js
@@ -29,6 +29,52 @@ export class RemoteService {
       data = "Please, Try Again";
     }
   }
+
+  async fetchFixtures() {
+    const uri = `https://greenethiopia.net/api/v1/fixtures`;
+    let response = await fetch(uri);
+    let res = await response.json();
+    if (response.status === 200) {
+      let data = res;
+      return fixturesFromJson(data);
+    } else {
+      data = "Please, Try Again";
+    }
+  }
+
+  async fetchTeams() {
+    const uri = `https://greenethiopia.net/api/v1/fpl`;
+    let response = await fetch(uri);
+    let res = await response.json();
+    if (response.status === 200) {
+      let data = res["obj"]["teams"];
+      return teamsFromJson(data);
+    } else {
+      data = "Please, Try Again";
+    }
+  }
+
+  // async fetchTotalPlayers(){
+  //     const uri = `https://greenethiopia.net/api/v1/fpl`;
+  //     let response = await fetch
+  // }
+
+  async fetchManagers() {
+    const uri = `https://greenethiopia.net/api/v1/managers`;
+    let response = await fetch(uri);
+    let res = await response.json();
+    let manager1;
+    let manager2;
+    let manager3;
+
+    if (response.status === 200) {
+      let data = res;
+      manager1 = managersFromJson(data["327713"]);
+      manager2 = managersFromJson(data["954118"]);
+      manager3 = managersFromJson(data["6292826"]);
+      return manager1;
+    }
+  }
 }
 
 // const baseURL = 'https://greenethiopia.net/api/v1/fpl';
