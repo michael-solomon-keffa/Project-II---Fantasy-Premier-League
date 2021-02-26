cd1a44f914d455a717ed44dbb78505cf784aac2c
Semere Talegngeta <se.semere.talegngeta@gmail.com>
2021-02-26 19:38:26 +0300

    gameweekfixtures integrated with difficulities and embleming

--- src/app.js
+++ src/app.js
@@ -80,30 +80,113 @@ let view = async function () {
 
     mosttransferredin.textContent = elements[events[gameweekpage].most_transferred_in - 1].first_name + " " + elements[events[gameweekpage].most_transferred_in - 1].second_name;
     mosttransferredinplayer.innerHTML = `<img src=${teams[elements[events[gameweekpage].most_transferred_in - 1].team - 1].jersey}>`;
-    mostcaptainedin.textContent =elements[events[gameweekpage].most_captained - 1].first_name +" " +elements[events[gameweekpage].most_captained - 1].second_name;
+    mostcaptainedin.textContent = elements[events[gameweekpage].most_captained - 1].first_name + " " + elements[events[gameweekpage].most_captained - 1].second_name;
     mostcaptainedinplayer.innerHTML = `<img src=${teams[elements[events[gameweekpage].most_captained - 1].team - 1].jersey}>`;
-  
-  
-  
+
+
+
     starplayertitle.textContent = `GW ${events[gameweekpage].id} star player`;
     starplayerjersey.innerHTML = `<img src=${teams[elements[events[gameweekpage].top_element - 1].team - 1].jersey}>`;
     starplayerfirstname.textContent = elements[events[gameweekpage].top_element - 1].first_name;
     starplayerlastname.textContent = elements[events[gameweekpage].top_element - 1].second_name;
     starplayerteam.textContent = "⚪ " + teams[elements[events[gameweekpage].top_element].team - 1].name;
     starplayerpoints.textContent = events[gameweekpage].top_element_info.points + " " + "points";
-  
+
     // Managers
     managername.textContent = managers[managerIdentity[authenicatedManager]].player_first_name + ' ' + managers[managerIdentity[authenicatedManager]].player_last_name;
     managernickname.textContent = managers[managerIdentity[authenicatedManager]].name;
     managerfavouriteteam.textContent = teams[managers[managerIdentity[authenicatedManager]].favourite_team - 1].name;
     manageroverallpoint.textContent = managers[managerIdentity[authenicatedManager]].summary_overall_points;
     managerrank.textContent = managers[managerIdentity[authenicatedManager]].summary_overall_rank;
-  
+
     overallpoint.textContent = managers[managerIdentity[authenicatedManager]].summary_overall_points;
-    
+
     gameweekscore.textContent = managers[managerIdentity[authenicatedManager]].history[24 - gameweekpage].points;
     gameweekonlyrank.textContent = numberWithCommas(managers[managerIdentity[authenicatedManager]].history[24 - gameweekpage].rank);
-  
+
+    profile.innerHTML = `<img  src="${teams[managers[managerIdentity[authenicatedManager]].favourite_team - 1].emblem}" alt="">`
+
+    // Leagues
+    let tableitems = '';
+    tableitems += `
+    <tr>
+      <th>League Name</th>
+      <th>Previous Rank</th>
+      <th>Current Rank</th>
+    </tr>
+  `
+
+    managers[managerIdentity[authenicatedManager]].leagues.classic.forEach((league) => {
+        tableitems += `
+      <tr>
+        <td class="info-left bottom-border" style="padding:10px;"><strong>${league.name}</strong></td>
+        <td class="info-highlight bottom-border" style="padding:10px;">${numberWithCommas(league.entry_last_rank)} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td class="info-highlight bottom-border ">${numberWithCommas(league.entry_rank)}</td>
+      </tr>
+    `
+    });
+    managers[managerIdentity[authenicatedManager]].leagues.h2h.forEach((league) => {
+        tableitems += `
+     <tr>
+       <td class="info-left bottom-border style="padding:10px;"><strong>${league.name}</strong></td>
+       <td class="info-highlight bottom-border" style="padding:10px;">${numberWithCommas(league.entry_last_rank)} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+       <td class="info-highlight bottom-border">${numberWithCommas(league.entry_rank)}</td>
+     </tr>
+   `
+    });
+    leagues.innerHTML = tableitems;
+
+    let homeDiv = "";
+
+    let gameweekfixtures = fixtures.filter((x) => {
+        return x.event === gameweekpage + 1;
+    });
+
+    let teamDifficulity = {
+        5: "#FFEBEE",
+        4: "#EF9A9A",
+        3: "#E57373",
+        2: "#F44336",
+        1: "#571C1C"
+    }
+
+    gameweekfixtures.forEach((x) => {
+        homeDiv += `<div class="row mt-4">
+            <div class="col-sm-12 fixturebox py-3">
+                <div class="row mt-2">
+                    <div class="col-sm-2 hometeam">
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <img src=${teams[x.team_h - 1].emblem}>
+                        </div>
+                        <div class="col-sm-12">
+                          <div style="width: 50px; height: 2px; background: ${teamDifficulity[x.team_h_difficulty]}"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-sm-2 versus text-center py-2">
+                        ${x.team_a_score !== null && x.team_h_score !== null ? x.team_h_score + '-' + x.team_a_score: 'V'}
+                    </div>
+                    <div class="col-sm-2 awayteam">
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <img src=${teams[x.team_a - 1].emblem}>
+                        </div>
+                        <div class="col-sm-12">
+                          <div style="width: 50px; height: 2px; background: ${teamDifficulity[x.team_a_difficulty]}"></div>
+                        </div>
+                      </div>                  
+                    </div>
+                    <div class="col-sm-5 fixtureplacetime">
+                        <div class="row">
+                            <div class="col-sm-12 fixturetime">${new Date(x.kickoff_time).getHours() +':'+ new Date(x.kickoff_time).getMinutes()}</div>
+                            <div class="col-sm-12 fixtureplace text-muted">${teams[x.team_h - 1].arena}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    });
 }
 
 view();
