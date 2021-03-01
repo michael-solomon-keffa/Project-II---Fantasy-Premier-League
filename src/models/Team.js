3cc3d73e9d47c64dd3f57956486c217a91d6788c
AbenezerAtnafu <se.abenezer.atnafu@gmail.com>
2021-03-01 19:18:49 +0300

    bug fixes in remote services and team

--- src/models/Team.js
+++ src/models/Team.js
@@ -123,3 +123,29 @@ export let teamsFromJson = (str) => {
   });
   return teams;
 };
+// const baseURL = 'https://greenethiopia.net/api/v1/fpl';
+// export const reqType = {
+//     bootstrap : '',
+// element : 'element-summary/',
+// events: 'events',
+// event: 'event',
+// entry: 'entry',
+// elementTypes: 'element-types',
+// gameweekFixtures: 'fixtures/?event',
+// teams: 'teams/',
+// leagueClassicStanding: 'leagues-classic'
+//}
+// const makeRequest = async (url, param) => {
+//     const response = await fetch(`${baseURL}${url}`);
+//     const res = await response.json();
+//     let data;
+//     if(response.status === 200){
+//         data = res['obj'];
+//     }
+//     return data;
+// }
+// // Get Overall Gameweek Information Data
+// export const getBootstrap = async () => {
+//       const data = makeRequest(reqType.bootstrap);
+//       return data;
+// }
